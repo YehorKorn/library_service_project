a0dac81d6223e33814f5ad861004cdabc2a1315b
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-21 12:47:29 +0300

    Added permissions for book

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 
 from books.models import Book
 from books.paginations import BookPagination
+from books.permissions import IsAdminOrIfAuthenticatedReadOnly
 from books.serializers import BookSerializer, BookListSerializer, BookDetailSerializer
 
 
@@ -9,6 +10,7 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     pagination_class = BookPagination
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     @staticmethod
     def _params_to_ints(qs):
