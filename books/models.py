59b9d7d672f367f9ab8c607c8e8ffc200df5d36f
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-21 10:46:51 +0300

    Added model author with str method and property 'full_name'

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=65,)
+    last_name = models.CharField(max_length=65,)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
