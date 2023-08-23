9a5cdb16b0add5321f4692a83c7b5c39abb51ba9
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-23 13:20:13 +0300

    Added blank=True for actual_return_date field. Changed __str__ method & method validate_date.

--- borrowings/models.py
+++ borrowings/models.py
@@ -11,17 +11,20 @@ from users.models import User
 class Borrowings(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True)
+    actual_return_date = models.DateField(null=True, blank=True)
     book: Book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user: User = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return (
-            f"Book: {self.book.title}; Borrow date: {self.borrow_date}\n"
-            f"Customer: {self.user.email}; Is active: {self.is_active}\n"
+        is_active_str = (
             f"Expected return date: {self.expected_return_date}"
             if self.is_active
             else f"Return date: {self.actual_return_date}"
+        )
+        return (
+            f"Book: {self.book.title}; Borrow date: {self.borrow_date};\n"
+            f"Customer: {self.user.email}; Is active: {self.is_active};\n"
+            + is_active_str
         )
 
     @property
@@ -31,13 +34,18 @@ class Borrowings(models.Model):
     @staticmethod
     def validate_date(expected_return_date, actual_return_date, error_to_raise):
         today = datetime.datetime.today().date()
-        for date_attr_name in [expected_return_date, actual_return_date]:
-            if not date_attr_name >= today:
-                raise error_to_raise(
-                    {
-                        date_attr_name: f"{date_attr_name} can't be any sooner than today."
-                    }
-                )
+        if not expected_return_date >= today:
+            raise error_to_raise(
+                {
+                    expected_return_date: f"{expected_return_date} can't be any sooner than today."
+                }
+            )
+        if actual_return_date and not actual_return_date == today:
+            raise error_to_raise(
+                {
+                    actual_return_date: f"{actual_return_date} cannot be earlier or later than today."
+                }
+            )
 
     def clean(self):
         Borrowings.validate_date(
