9e40a1adfe6abcdc2cc46845dc61f9dfb7408027
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-28 16:12:39 +0300

    Refactor to best practice

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,6 @@ router = routers.DefaultRouter()
 
 router.register("", BookViewSet, basename="books")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
 
 app_name = "books"
